32a7a8f8dcb79b47867e3a3bc9c59a095db5f2d5
Karl Fredrik Aalberg <aalberg@me.com>
2025-05-10 00:11:01 +0200

    Auto-commit 2025-05-10 00:11:01

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,16 +1,28 @@
 return {
   "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
   opts = {
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
+    format_on_save = false,
+
     formatters_by_ft = {
       lua = { "stylua" },
-      python = { "black" },
       javascript = { "prettier" },
       typescript = { "prettier" },
-      -- add more as needed
+      json = { "prettier" },
+      python = { "black" },
+    },
+
+    -- Formatter-specific options
+    formatters = {
+      prettier = {
+        prepend_args = { "--tab-width", "2", "--use-tabs", "false" },
+      },
+      stylua = {
+        prepend_args = { "--indent-width", "2", "--indent-type", "Spaces" },
+      },
+      black = {
+        prepend_args = { "--line-length", "88" }, -- optional: default is 88
+      },
     },
   },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,45 @@
 return {
   "neovim/nvim-lspconfig",
-  config = function()
-    local lspconfig = require("lspconfig")
-    lspconfig.lua_ls.setup({})
-    lspconfig.ts_ls.setup({})
-    lspconfig.pyright.setup({})
-  end,
   dependencies = {
+    "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
   },
+  build = ":MasonUpdate",
+  config = function()
+    require("mason").setup()
+
+    require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls", "ts_ls", "pyright" },
+      automatic_installation = true,
+    })
+
+    local lspconfig = require("lspconfig")
+
+    local on_attach = function(client, bufnr)
+      -- Disable LSP formatting if using conform.nvim
+      client.server_capabilities.documentFormattingProvider = false
+      client.server_capabilities.documentRangeFormattingProvider = false
+
+      -- Your LSP keymaps here
+      local map = function(mode, lhs, rhs)
+        vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true })
+      end
+
+      map("n", "gd", vim.lsp.buf.definition)
+      map("n", "gD", vim.lsp.buf.declaration)
+      map("n", "gr", vim.lsp.buf.references)
+      map("n", "gi", vim.lsp.buf.implementation)
+      map("n", "K", vim.lsp.buf.hover)
+      map("n", "<C-k>", vim.lsp.buf.signature_help)
+      map("n", "<leader>rn", vim.lsp.buf.rename)
+      map("n", "<leader>ca", vim.lsp.buf.code_action)
+      map("n", "<leader>e", vim.diagnostic.open_float)
+      map("n", "[d", vim.diagnostic.goto_prev)
+      map("n", "]d", vim.diagnostic.goto_next)
+    end
+    -- Set up each LSP server
+    lspconfig.lua_ls.setup({ on_attach = on_attach })
+    lspconfig.ts_ls.setup({ on_attach = on_attach })
+    lspconfig.pyright.setup({ on_attach = on_attach })
+  end,
 }
