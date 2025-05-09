a86298af8ab9c8717ffa465335ebdf59f7decfc3
Karl Fredrik Aalberg <aalberg@me.com>
2025-05-09 14:05:43 +0200

    Auto-commit 2025-05-09 14:05:43

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,11 +1,11 @@
 return {
-  "williamboman/mason.nvim",
-  build = ":MasonUpdate",
-  config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "lua_ls", "ts_ls", "pyright" },
-      automatic_installation = true,
-    })
-  end,
+	"williamboman/mason.nvim",
+	build = ":MasonUpdate",
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = { "lua_ls", "ts_ls", "pyright" },
+			automatic_installation = true,
+		})
+	end,
 }
